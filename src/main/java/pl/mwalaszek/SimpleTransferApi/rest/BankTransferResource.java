a91a75175a2b00801a64d99d1ab29750a91d6c86
mwalaszek <walaszek.marcin@gmail.com>
2018-12-27 17:30:43 +0100

    changed bank transfer resource to return executed transfer

--- src/main/java/pl/mwalaszek/SimpleTransferApi/rest/BankTransferResource.java
+++ src/main/java/pl/mwalaszek/SimpleTransferApi/rest/BankTransferResource.java
@@ -1,16 +1,14 @@
 package pl.mwalaszek.SimpleTransferApi.rest;
 
+import pl.mwalaszek.SimpleTransferApi.model.Transfer;
 import pl.mwalaszek.SimpleTransferApi.service.AccountsService;
 import pl.mwalaszek.SimpleTransferApi.service.TransferService;
-import pl.mwalaszek.SimpleTransferApi.model.Account;
-import pl.mwalaszek.SimpleTransferApi.model.Transfer;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Collection;
 
 @Path("/transfers")
 public class BankTransferResource {
@@ -25,8 +23,8 @@ public class BankTransferResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<Account> executeBankTransfer(Transfer transfer) {
+    public Transfer executeBankTransfer(Transfer transfer) {
         transferService.executeTransfer(transfer);
-        return accountsService.findAll();
+        return transfer;
     }
 }
